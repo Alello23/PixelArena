a46bf847d35743a6f198b38ec910808e2c1f9dd3
joaquim <joaquim.balletbo@students.salle.url.edu>
2024-01-06 21:25:59 +0100

    joaquim  començament shop

--- project-base/src/components/DropDownShopComponent.vue
+++ project-base/src/components/DropDownShopComponent.vue
@@ -1,55 +1,40 @@
 <template>
-  <div class="custom-button" style="background-color: #DECA91;">
-    <button
-      type="button"
-      class="btn btn-danger dropdown-toggle"
-      style="background-color: #ffffff; border: none;"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      <h3 style="color: black;">{{ selectedAttack || label }}</h3>
-    </button>
-    <ul class="dropdown-menu dropdown-menu-right">
-                <li @click="selectAttack('UP')"><a class="dropdown-item" href="#U">UP</a></li>
-                <li @click="selectAttack('DOWN')"><a class="dropdown-item" href="#D">DOWN</a></li>
-                <li @click="selectAttack('LEFT')"><a class="dropdown-item" href="#L">LEFT</a></li>
-                <li @click="selectAttack('RIGHT')"><a class="dropdown-item" href="#R">RIGHT</a></li>
-    </ul>
+  <div class="space_between">
+    <div class="col rounded-column-2 text-center" style="background-color: #DECA91;">
+      <h4>Equipped attacks</h4>
+    </div>
+  </div>
+
+  <div v-for="option in dropdownOptions" :key="option" class="space_between" :style="{ marginRight: margins, marginLeft: margins }">
+    <div class="col rounded-column-2 text-center">
+      <h5>{{ getOptionName(option) }}</h5>
+    </div>
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex';
-
 export default {
   props: {
-    label: String,
-    dropdownId: String,
-  },
-  computed: {
-    selectedAttack() {
-      return this.selectedAttackValue;
-    },
-    backpackedAttacks() {
-      const store = useStore();
-      // Retrieve backpacked attacks from the store or modify based on your store structure
-      return store.state.backpackedAttacks;
-    },
-   },
-  methods: {
-    selectAttack(attack) {
-      this.selectedAttackValue = attack;
-    this.$emit('attack-selected', { dropdownId: this.dropdownId, attack}); //TODO: aixo es per enviar al component que conte aixo REFERENCIA: Change equippedattacks
-    },
-    getSelectedAttackFromStore() {
-    // Use the store to get the initial selected attack value
-    return this.$store.getters.getSelectedAttack(this.dropdownId) || null;
-  },
+    margins: String,
   },
   data() {
     return {
-      selectedAttackValue: this.getSelectedAttackFromStore(),
+      dropdownOptions: [
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 0, y: -1 },
+        { x: -1, y: 0 },
+        { x: -1, y: -1 },
+        { x: -1, y: 1 },
+        { x: 1, y: -1 },
+      ],
     };
+  },
+  methods: {
+    getOptionName(option) {
+      return `(${option.x},${option.y})`;
+    },
   },
 };
 </script>
--- project-base/src/components/MenuComponent.vue
+++ project-base/src/components/MenuComponent.vue
@@ -2,6 +2,7 @@
 import ButtonComponentColor from '../components/ButtonComponentColor.vue'
 import DropDownComponent from '../components/DropDownComponent.vue';
 import AddData from './AddData.vue';
+import CreateAttackComponent from '../components/CreateAttackComponent.vue';
 </script>
 
 
@@ -206,66 +207,7 @@ import AddData from './AddData.vue';
                 </div>
             </div>
             <!-- Part del create -->
-            <div class="modal fade" tabindex="-1" id="createAttack">
-                <div class="modal-dialog modal-dialog-centered modal-lg">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="col self-align-center">
-                                <div class="col" >
-                                    <div class="row">
-                                    <div class="col rounded-column-2 text-center" style="background-color: #DECA91;" >
-                                        <h1>Create attack</h1>
-                                        <div class="col"> 
-                                        <div class="space_between" style="margin-right: 60px; margin-left: 60px; ">
-                                            <div class="custom-button"  style="background-color: #DECA91;">
-                                            <AddData placeholderText="Add name to the attack" />
-                                            </div>
-                                        </div>
-                                        <div class="space_between" style="margin-right: 200px; margin-left: 200px; ">
-                                            <DropDownShopComponent label="Position"></DropDownShopComponent>
-                                        </div> 
-                                    </div> 
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                                <div class="col-2" >
-                                    <div class="space_between">
-                                    </div>
-                                </div>
-                                <div class="col" >
-                                <div class="row">
-                                    <div class="col-2 ">
-                                        <div class="space_between" >
-                                        </div>
-                                    </div>
-                                    <div class="col-3 ">
-                                        <div class="space_between" >
-                                            <button type="button" class="custom-button" style="background-color: #419FD6;border: 4px solid #000; box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2); padding: 20px 0; line-height: 2; color: white;" data-bs-target="#shop" data-bs-toggle="modal"> <h4>Cancel</h4></button>
-                                            </div>
-                                    </div>
-                                    <div class="col-2 ">
-                                        <div class="space_between" >
-                                        </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="space_between" >
-                                            <button type="button" class="custom-button" style="background-color: #419FD6;border: 4px solid #000; box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2); padding: 20px 0; line-height: 2; color: white;" data-bs-target="#shop" data-bs-toggle="modal"> <h4>Create</h4></button>
-                                            </div>
-                                    </div>
-                                    <div class="col-2 ">
-                                        <div class="space_between" >
-                                        </div>
-                                    </div>
-                                </div> 
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
+            <CreateAttackComponent id="createAttack"></CreateAttackComponent>
             <!-- Part del confirm -->
             <div class="modal fade" tabindex="-1" id="buyConfirmation">
             <div class="modal-dialog modal-dialog-centered modal-lg">
