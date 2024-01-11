89b6e06ef2a55501500857ed0725bb4ce7df15d9
joaquim <joaquim.balletbo@students.salle.url.edu>
2024-01-11 16:21:35 +0100

    joaquim sell attack fet

--- project-base/src/components/SellAttackComponent.vue
+++ project-base/src/components/SellAttackComponent.vue
@@ -1,6 +1,95 @@
 <script setup>
 import DropDownComponent from '../components/DropDownComponent.vue';
-import AddData from './AddData.vue';
+import InputComponent from '../components/InputComponent.vue';
+import { onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { ref } from 'vue';
+
+const store = useStore();
+const PriceVariable = ref('');
+
+const selectedAttacks = ref({ dropdown: null});
+
+const handleSaveAttacks = (payload) => {
+    console.log('Selected Attack Payload:', payload);
+  // Access payload.dropdownId and payload.attack here
+  const { dropdownId, attack } = payload;
+
+  // Update the selectedAttacks variable based on which dropdown emitted the payload
+  selectedAttacks.value[dropdownId] = attack;
+}; 
+const handleInput2 = (value) => {
+    PriceVariable.value = value;
+    console.log('Saved PriceVariable: ', PriceVariable.value);
+
+};
+const sellAttack = async () => {
+  try {
+    const { dropdown } = selectedAttacks.value;
+    const apiUrl = `https://balandrau.salle.url.edu/i3/shop/attacks/${dropdown.attack_ID}/sell`;
+    const token = store.getters.getplayer.token;
+    console.log('The value of the token is: ', token);
+    console.log('Passed Attacks: ', dropdown.attack_ID);
+    // Parse the value as an integer
+    const priceValue = parseInt(PriceVariable.value, 10);
+    console.log('Passed PriceVariable: ', priceValue);
+    const requestData = {
+      price: priceValue,
+    };
+    const headers = {
+      'Bearer': token,
+      'Content-Type': 'application/json',
+    };
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData),
+    });
+
+    if (response.ok) {
+      console.log('Attack put on sale successfully');
+    } else {
+      console.error('Failed to sell Attack ');
+      // Handle the error if needed
+    }
+  } catch (error) {
+    console.error('Error during attack selling:', error);
+    // Handle the error if needed
+  }
+};
+// Fetch attacks from the API when the component is mounted
+onMounted(async () => {
+  try {
+    const apiUrl = 'https://balandrau.salle.url.edu/i3/players/attacks';
+    const token = store.getters.getplayer.token;
+    const headers = {
+      'Bearer': token,
+      'Content-Type': 'application/json',
+    };
+
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: headers,
+    });;
+
+    if (response.ok) {
+      store.dispatch('clearAllBackpackedAttacks');
+      const attacks = await response.json();
+      // Dispatch the attacks to the store
+      attacks.forEach((backpackedAttacks) => {
+        if (backpackedAttacks.equipped != true) {
+          store.dispatch('addBackpackedAttack', backpackedAttacks);
+        } 
+        
+      });
+    } else {
+      console.error('Failed to fetch attacks');
+    }
+  } catch (error) {
+    console.error('Error during attack fetch:', error);
+    // Handle the error if needed
+  }
+});
 </script>
 <template>
 <div class="modal fade" tabindex="-1" :id="id">
@@ -17,11 +106,13 @@ import AddData from './AddData.vue';
               <h1>Sell an attack</h1>
               <div class="col">
                   <div class="space_between" style="margin-right: 60px; margin-left: 60px;">
-                  <DropDownComponent>Select the attack you want to sell</DropDownComponent>
+                    <h5>Select the attack you want to sell</h5>
+                  <DropDownComponent  label="" dropdownId="dropdown" @attack-selected="handleSaveAttacks"></DropDownComponent>
                   </div>
                   <div class="space_between" style="margin-right: 60px; margin-left: 60px;">
                   <div class="custom-button" style="background-color: #DECA91;">
-                      <AddData placeholderText="Set a price" />
+                    <InputComponent label="Set a price" v-model="PriceVariable"
+                                                :maxCharacters="21" @update:parentValue="handleInput2"></InputComponent>
                   </div>
                   </div>
               </div>
@@ -50,7 +141,7 @@ import AddData from './AddData.vue';
           </div>
           <div class="col-3">
               <div class="space_between">
-              <button type="button" class="custom-button" style="background-color: #419FD6;border: 4px solid #000; box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2); padding: 20px 0; line-height: 2; color: white;" :data-bs-target="data_bs_target" data-bs-toggle="modal"> <h4>Sell</h4></button>
+              <button type="button" class="custom-button" style="background-color: #419FD6;border: 4px solid #000; box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2); padding: 20px 0; line-height: 2; color: white;" :data-bs-target="data_bs_target" data-bs-toggle="modal" @click="sellAttack"> <h4>Sell</h4></button>
               </div>
           </div>
           <div class="col-2 ">
